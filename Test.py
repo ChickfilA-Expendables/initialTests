c739d9441024e87d7e93b5a18e1f59d89af53b8a
sbhatnagar30 <sbhatnagar30@gatech.edu>
2018-11-30 15:30:35 -0500

    added stuff

--- Test.py
+++ Test.py
@@ -3,11 +3,33 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import picamera
+import requests as req
+import sys
 
-camera = picamera.PiCamera()
-camera.capture('image.jpg')
+lemonadeURL = 'https://young-anchorage-97125.herokuapp.com/data/1.json'
+ketchupURL = 'https://young-anchorage-97125.herokuapp.com/data/3.json'
+
+def expendableData(expendable, value):
+    if expendable == 'lemonade':
+        print('Sending Lemonade Data to Server...')
+        url = lemonadeURL
+    elif expendable == 'ketchup':
+        print('Sending Ketchup Data to Server...')
+        url = ketchupURL
+    else:
+        print('Not a Valid Expendable')
+        return None
+    payload = {'value' : value}
+    r = req.patch(url, payload)
+    if r.status_code >= 200 and r.status_code < 300:
+        print('Successfully updated')
+    else:
+        print('Status Code:' + r.status_code)
+
+#camera = picamera.PiCamera()
+#camera.capture('image.jpg')
 #img = cv2.imread('../trainingData/frames/frame0.jpg')
-img = cv2.imread('image.jpg')
+img = cv2.imread('pictures/image2.jpg')
 
 # Prepocess
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -28,11 +50,7 @@ cv2.drawContours(stencil, [contours[listindex[card_number]]], 0, (255, 255, 255)
 res = cv2.bitwise_and(img, stencil)
 cv2.imwrite("out.bmp", res)
 
-
 #print res.shape
-plt.figure(1)
-plt.imshow(res)
-#plt.show()
 x = res.shape[0]
 y = res.shape[1]
 
@@ -41,48 +59,55 @@ y_red = []
 y_blue = []
 y_green = []
 avg = []
+diff = []
 sample_r = res[x/2][y/2]
 print sample_r
 for i in range(x):
-  avg.append(int(res[i][y/2][0]*(1-float(sample_r[0])/float(255)) + res[i][y/2][1]*(1-float(sample_r[1])/float(255)) + res[i][y/2][2]*(1-float(sample_r[2])/float(255))))
+  diff.append(((sample_r[0] - res[i][y/2][0]) + (sample_r[1] - res[i][y/2][1]) + (sample_r[2] - res[i][y/2][2])))
+  res[i][y/2] = [255,0,0]
+  #avg.append(int(res[i][y/2][0]*(1-float(sample_r[0])/float(255)) + res[i][y/2][1]*(1-float(sample_r[1])/float(255)) + res[i][y/2][2]*(1-float(sample_r[2])/float(255))))
   y_red.append(res[i][y/2][0])
   y_green.append(res[i][y/2][1])
   y_blue.append(res[i][y/2][2])
+
+plt.figure(1)
+plt.imshow(res)
+#plt.show()
+
 
 #for i in range(x):
   #print (y_red[i] * sample_r[0]/255) #+ y_green[i] * sample_r[1]/255 + y_blue[i] * sample_r[2]/255)
 
 container = 0.0
 break_condition = 0
-for i in avg:
+for i in y_red:
   if (i != 0):
     container = container + 1
   else:
     break_condition = break_condition + 1
   if (break_condition > 5):
     break
-maxValue = 0.0
-for i in t:
-  if (avg[i] > maxValue):
-    maxValue = i
-print maxValue
-percant = maxValue / container * 100
+#maxValue = 0.0
+#for i in t:
+#  if (avg[i] > maxValue):
+#    maxValue = i
+#print maxValue
+#percant = maxValue / container * 100
 #percant = 100 - percant
-if (percant > 100):
-  percant = 0
+#if (percant > 100):
+#  percant = 0
 
-print "lemonade level = ",percant
+#print "lemonade level = ",percant
 print "container length = ",container
-
 plt.figure(2)
-plt.plot(t,y_red)
-plt.figure(3)
-plt.plot(t,y_green)
-plt.figure(4)
-plt.plot(t,y_blue)
-plt.figure(5)
-plt.plot(t,avg)
-plt.show()
-
+plt.plot(t,diff)
+#plt.figure(3)
+#plt.plot(t,y_green)
+#plt.figure(4)
+#plt.plot(t,y_blue)
+#plt.figure(5)
+#plt.plot(t,avg)
+#plt.show()
+expendableData('lemonade', 65)
 #canny = cv2.Canny(res, 100, 200)
 #cv2.imwrite("canny.bmp", canny)
