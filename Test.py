829fb11798825d694fad2e63928472b6007f54ec
sbhatnagar30 <sbhatnagar30@gatech.edu>
2018-12-08 14:36:32 -0500

    finished

--- Test.py
+++ Test.py
@@ -51,9 +51,13 @@ def calculateLevel(res):
   x = res.shape[0]
   y = res.shape[1]
 
+  global t
   t = range(0,x)
+  global y_red 
   y_red = []
+  global y_blue 
   y_blue = []
+  global y_green 
   y_green = []
   avg = []
   diff = []
@@ -64,37 +68,39 @@ def calculateLevel(res):
   thresh = 3
   for i in range(x):
     diff.append(abs((int(sample_r[0]) - int(res[i][y/2][0])) + (int(sample_r[1]) - int(res[i][y/2][1])) + (int(sample_r[2]) - int(res[i][y/2][2]))))
-    #y_red.append(abs(int(sample_r[0]) - int(res[i][y/2][0])))
-    #y_green.append(abs(int(sample_r[1]) - int(res[i][y/2][1])))
-    #y_blue.append(abs(int(sample_r[2]) - int(res[i][y/2][2])))
+    y_red.append(abs(int(sample_r[0]) - int(res[i][y/2][0])))
+    y_green.append(abs(int(sample_r[1]) - int(res[i][y/2][1])))
+    y_blue.append(abs(int(sample_r[2]) - int(res[i][y/2][2])))
     red = abs(int(sample_r[0]) - int(res[i][y/2][0]))
     green = abs(int(sample_r[1]) - int(res[i][y/2][1]))
     blue = abs(int(sample_r[2]) - int(res[i][y/2][2]))
-    difference = abs((int(sample_r[0]) - int(res[i][y/2][0])) + (int(sample_r[1]) - int(res[i][y/2][1])) + (int(sample_r[2]) - int(res[i][y/2][2])))
-    if (difference < 15):
+    difference = abs(abs(int(sample_r[0]) - int(res[i][y/2][0])) + abs(int(sample_r[1]) - int(res[i][y/2][1])) + abs(int(sample_r[2]) - int(res[i][y/2][2])))
+    if (difference < 40):
       level_diff+= 1
-    if (red < 9):
+    if (red < 40):
       level_r += 1
-    if (green < 7):
+    if (green < 30):
       level_g += 1
-    if (blue < 5):
+    if (blue < 30):
       level_b += 1
-    y_red.append(res[i][y/2][0])
-    y_green.append(res[i][y/2][1])
-    y_blue.append(res[i][y/2][2])
+    #y_red.append(res[i][y/2][0])
+    #y_green.append(res[i][y/2][1])
+    #y_blue.append(res[i][y/2][2])
     #res[i][y/2] = [255,0,0]
-  container = 0.0
+  global container
+  container = 800.0
   break_condition = 0
   for i in y_red:
     if (i != 0):
-      container = container + 1
+      container = 300.0 #container + 1
     else:
       break_condition = break_condition + 1
     if (break_condition > 5):
       break
+  print container
   if (container == 0):
     container = 1
-  percant = level_diff / container * 100
+  percant = level_r / container * 100
   return [level_r, level_g, level_b, level_diff, percant]
 
 
@@ -137,13 +143,25 @@ plt.ion()
 
 counter = 0
 avg = 0
+#plt.figure(1)
+#plt.imshow(img2)
+#plt.figure(2)
+#plt.plot(t,y_green)
+#plt.figure(3)
+#plt.plot(t,y_blue)
+#plt.figure(4)
+#plt.plot(t,y_red)
+#plt.show()
+
 
 while True:
-  counter += 1
   im1.set_data(takePicture2())
   im2.set_data(takePicture1())
-  avg = (avg+calc[4]) / counter
-  if (counter > 5):
+  if (container>100):
+    counter += 1
+    avg = (avg+calc[4])
+  if (counter > 1):
+    avg = avg / counter
     if (avg > 100):
       avg = 100
     if (avg < 0):
@@ -182,14 +200,5 @@ plt.show()
 #print "level_b = ",level_b
 #print "level_diff = ",level_diff
 #print "percant = ",percant
-#plt.figure(2)
-#plt.plot(t,diff)
-#plt.figure(3)
-#plt.plot(t,y_green)
-#plt.figure(4)
-#plt.plot(t,y_blue)
-#plt.figure(5)
-#plt.plot(t,y_red)
-#plt.show()
 #canny = cv2.Canny(res, 100, 200)
 #cv2.imwrite("canny.bmp", canny)
